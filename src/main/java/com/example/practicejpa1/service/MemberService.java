b78ce6521909a1111456f6713562bcb6c5274bc1
AirIHL <inhak1122@gmail.com>
2026-01-02 21:15:41 +0900

    Basic Service

---
+++ src/main/java/com/example/practicejpa1/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.example.practicejpa1.service;
+
+import com.example.practicejpa1.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+}
