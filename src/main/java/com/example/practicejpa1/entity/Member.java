e136ed73be5425164219e4fd3ec832eca6ce65e9
AirIHL <inhak1122@gmail.com>
2026-01-02 18:21:03 +0900

    Maked Entity

--- src/main/java/com/example/practicejpa1/entity/Member.java
+++ src/main/java/com/example/practicejpa1/entity/Member.java
@@ -1,4 +1,22 @@
 package com.example.practicejpa1.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Member(String name) {
+        this.name = name;
+    }
 }
